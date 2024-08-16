6d2686e7dbaf6d4b07564e384be0e5346cebee9b
Cliffp15 <cliftonp15@yahoo.com>
2024-08-16 04:25:49 -0400

    Set body color to darker color, used "Home" img for lemo backdrop", added opacity and z index, brigthen navbar color

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -7,7 +7,7 @@ function HelloWorld() {
       <div className="Hero">
         <h2>Welcome to the future of living better</h2>
         <h1>Organize Your Life Better</h1>
-        {/* <img className="Hero-img" src={lemon} alt="dog" /> */}
+        <img className="Hero-img" src={lemon} alt="dog" />
       </div>
       <p>This is a simple Home Page</p>
 
