3f7fe78d35f1e00ac4b39d8488a307e6eedc0aa4
Clifton Patterson <Cliftonp15@yahoo.com>
2024-09-04 22:59:42 -0400

    Add features section components to home page

--- src/Component/Features.js
+++ src/Component/Features.js
@@ -3,18 +3,21 @@ import React from 'react';
 const Features = () => {
     return (
         <div>
-            <section className='Features-Container'>
-            <div className="Feature-Intro-Section">
-        <h2>Features</h2>
-        <p>Organize your life better with our features</p>
+    <section className='Features-Container'>
+        <div className="Feature-Intro-Section">
+            <div className="Feature-Description">
+            <h2>Features</h2>
+            <p>Novi is a lifestyle website designed to help you improve productivity, achieve goals, and cultivate better health.</p>
+            <p>We provide tools and guidance to help you organize your life, focus on what matters, and make steady progress towards your objectives.</p>
+            
+            </div>
         </div>
     
-    <div className="Feature-Section">
-    <p>This is a simple Home Page</p>
-
-    <p>Click on the links above to navigate to different pages</p>
+        <div className="Feature-Services">
+            <p>This is a simple Home Page.</p>
+            <p>Click on the links above to navigate to different pages</p>
         </div>
-        </section>
+    </section>
         
         { /* <div className="Feature-Intro-Section">
         <h2>Features</h2>
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,20 +1,23 @@
 import React from "react";
 import lemon from "../Images/Lemon.png";
+import Features from "../Component/Features";
+
 
 function HelloWorld() {
   return (
     <div>
       <section>
-   
+  
       <div className="Hero">
         <h2>Welcome to the future of <br></br>living better</h2>
-        <h1>Organize Your Life Better</h1>
+        <h1>Organize <br></br>Your Life <br></br>Better</h1>
         <img className="Hero-img" src={lemon} alt="dog" />
         <div className="Hero-Buttons">
-        <div><button>Get Started</button></div>
-        {/* <div><button>Get Started</button></div> */}
+          <div><button>Get Started</button></div>
         </div>
+        
       </div>
+      <Features />
       </section>
       
     </div>
