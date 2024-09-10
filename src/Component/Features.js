78777df774ed5c78b5ae679e8e296889c7360962
Clifton Patterson <Cliftonp15@yahoo.com>
2024-09-10 00:18:59 -0400

    rename features divs to match sections titles, set features description and image sections to be similar sizes and correct spacing problem on button near hero section that made black gap

--- src/Component/Features.js
+++ src/Component/Features.js
@@ -7,47 +7,44 @@ import career from "../Images/businesswoman-8676522.jpg";
 const Features = () => {
   return (
     <div>
-      <section className="Features-Container">
-        <div className="Site-Intro-Section">
-          <div className="Site-Description">
+      <section className="Features-Section">
+        <div className="Features-Container">
+          <div className="Features-Description">
+            <div className="Features-Title">
             <h2>
               Productivity <br></br> Website
             </h2>
+            </div>
+            <div className="Features-Break">
             <hr></hr>
+            </div>
+            <div className="Features-Paragraph">
             <p>
               Novi is a lifestyle website designed to help you improve
               productivity, achieve goals, and cultivate better health.
             </p>
+            </div>
+            <div className="Features-Paragraph">
             <p>
               We provide tools and guidance to help you organize your life,
               focus on what matters, and make steady progress towards your
               objectives.
             </p>
+            </div>
           </div>
-          <div className="Site-Image">
-            <img className="Feature-img" src={setup} alt="dog" />
+          <div className="Site-Image-Container">
+            <div className="Site-Image">
+              <img className="Feature-img" src={setup} alt="dog" />
+            </div>
+            <div className="Site-Image">
+              <img className="Feature-img" src={career} alt="dog" />
+            </div>
+            <div className="Site-Image">
+              <img className="Feature-img" src={food} alt="dog" />
+            </div>
           </div>
-          <div className="Site-Image">
-            <img className="Feature-img" src={career} alt="dog" />
-          </div>
-          <div className="Site-Image">
-            <img className="Feature-img" src={food} alt="dog" />
-          </div>
-          
         </div>
       </section>
-
-      {/* <div className="Feature-Intro-Section">
-        <h2>Features</h2>
-        <p>Organize your life better with our features</p>
-      </div> */}
-      {/* <div className="Feature-Section">
-      <p>This is a simple Home Page</p>
-
-      <p>Click on the links above to navigate to different pages</p>
-
-      <p>Click on the links above to navigate to different pages</p>
-   </div> */}
     </div>
   );
 };
